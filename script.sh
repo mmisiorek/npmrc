30e1587587bcc34782f63abe149204b79645813c
Marcin Misiorek <marcin@misiorek.net>
2021-09-15 20:59:00 +0200

    Add working script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-echo "hello from my world!"
-touch ~/some_file 
+if [[ "$PACKAGE" == "" ]]; then
+  NPMRC_PATH="~/.npmrc"
+else
+  NPMRC_PATH="packages/$PACKAGE/.npmrc"
+fi
+
+if [[ -f "$NPMRC_PATH" ]]; then
+  rm "$NPMRC_PATH"
+fi
+
+if [[ -n "$ORGANIZATION_NAME" ]]; then
+  echo "registry = https://npm.pkg.github.com/$ORGANIZATION_NAME" >> "$NPMRC_PATH"
+fi
+
+echo "//npm.pkg.github.com/:_authToken=$GITHUB_PERSONAL_TOKEN" >> "$NPMRC_PATH"
